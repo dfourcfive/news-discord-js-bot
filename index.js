d4ee619d1d1e94b3d7f472acefdc3eb2832cfc2c
dfourcfive <i.benadidou@esi-sba.dz>
2022-11-15 12:02:21 +0100

    add args option for the command lang and country

--- index.js
+++ index.js
@@ -13,6 +13,8 @@ const client = new Client({ partials: ["CHANNEL", "MESSAGE"],intents: [
 ] });
 
 
+const args=['country','lang'];
+
 client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}!`);
   });
@@ -20,25 +22,53 @@ client.on('ready', () => {
 
   
 client.on("messageCreate", message => {
-    console.log(message.cleanContent);
-    console.log("messageCreate");
-    if (message.content === "$New") {
-      getNews().then( async news =>  {
+
+
+   if (message.content.startsWith("$New")) {
+    
+      if(isCommandeWithArgs(message.content)){
+        let country = getArgumentByKey('country',message.content);
+        let lang = getArgumentByKey('lang',message.content);
+        getNews(country,lang).then( async news =>  {
           if(news != null){
-            if(news['image_url'] != null){
-              await message.channel.send(news['image_url']+"\n"+news['title']);
+            if(news['link'] != null){
+              await message.channel.send(news['link']+"\n"+news['title']+"\n"+news['description']);
+            }else{
+              await message.channel.send(news['title']);
+            }
+          }
+      });
+      }else{
+        getNews().then( async news =>  {
+          if(news != null){
+            if(news['link'] != null){
+              await message.channel.send(news['link']+"\n"+news['title']+"\n"+news['description']);
             }else{
               await message.channel.send(news['title']);
             }
           }
       });
       }
+      
+      }
 });
+
 
 client.login(process.env.TOKEN);
 
-  function getNews(){
-    const url=`https://newsdata.io/api/1/news?apikey=${process.env.KEY}&country=au,ca,fr,us&language=en`;
+  function getNews(country,lang){
+    let url=`https://newsdata.io/api/1/news?apikey=${process.env.KEY}`;
+    if(country != null){
+      url=url+"&country="+country;
+    }else{
+      url=url+"&country=ca,us";
+    }
+
+    if(lang != null){
+      url=url+"&language="+lang;
+    }else{
+      url=url+"&language=en,fr";
+    }
 
     return got(url, { json: true }).then(response  => {
         return response.body
@@ -47,4 +77,28 @@ client.login(process.env.TOKEN);
     }).catch(error => {
       console.log(error);
     });
+  }
+
+
+  function getArgumentByKey(key ,message){
+    var argumentValue;
+    if(message.includes(key)){
+      let values = message.split('-');
+      for (let index = 0; index < values.length; index++) {
+        if(values[index].includes(key)){
+          argumentValue=  values[index].substring(key.length).replace(' ', '');
+        }
+      }
+    }
+    return argumentValue;
+  }
+
+  function isCommandeWithArgs(message){
+    var argumentValue=false;
+    for (let index = 0; index < args.length; index++) {
+      if(message.includes(args[index])){
+        argumentValue=  true;
+      }
+    }
+    return argumentValue;
   }
